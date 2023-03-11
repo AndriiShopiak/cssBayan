a5b4ddee4b5a9386e2d2918947be3340053ad701
Andrii Shopiak <andriyshopyak@gmail.com>
2023-03-11 10:52:05 +0200

    feat: add close rotate  (Sat, Mar 11, 2023 10:50:30)

--- cssBayan/js/main.js
+++ cssBayan/js/main.js
@@ -3,12 +3,15 @@ let closeSvg = document.querySelectorAll("svg");
 for (let i = 1; i < memesBlock.length; i++) {
     memesBlock[i].addEventListener('mouseenter', () => {
         memesBlock[i].style.height = '500px';
+        closeSvg[i].style.transform = 'rotate(45deg)';
     });
     memesBlock[i].addEventListener('mouseleave', () => {
         memesBlock[i].style.height = '44px';
+        closeSvg[i].style.transform = 'rotate(0deg)';
     });
     closeSvg[i].addEventListener('click', () => {
         memesBlock[i].style.height = '44px';
+        closeSvg[i].style.transform = 'rotate(0deg)';
     });
 }
 closeSvg[0].addEventListener('click', () => {
@@ -17,12 +20,15 @@ closeSvg[0].addEventListener('click', () => {
     for (let i = 0; i < memesBlock.length; i++) {
         memesBlock[i].addEventListener('mouseenter', () => {
             memesBlock[i].style.height = '500px';
+            closeSvg[i].style.transform = 'rotate(45deg)';
         });
         memesBlock[i].addEventListener('mouseleave', () => {
             memesBlock[i].style.height = '44px';
+            closeSvg[i].style.transform = 'rotate(0deg)';
         });
         closeSvg[i].addEventListener('click', () => {
             memesBlock[i].style.height = '44px';
+            closeSvg[i].style.transform = 'rotate(0deg)';
         });
     }
 });
