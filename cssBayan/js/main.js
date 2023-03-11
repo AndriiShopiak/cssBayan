afb0954173cd404790d683071676dd28120fe96f
Andrii <andriyshopyak@gmail.com>
2023-03-11 10:25:19 +0200

    feat: add close function (Sat, Mar 11, 2023 10:24:34)

---
+++ cssBayan/js/main.js
@@ -0,0 +1,28 @@
+let memesBlock = document.querySelectorAll(".memes__content");
+let closeSvg = document.querySelectorAll("svg");
+for (let i = 1; i < memesBlock.length; i++) {
+    memesBlock[i].addEventListener('mouseenter', () => {
+        memesBlock[i].style.height = '500px';
+    });
+    memesBlock[i].addEventListener('mouseleave', () => {
+        memesBlock[i].style.height = '44px';
+    });
+    closeSvg[i].addEventListener('click', () => {
+        memesBlock[i].style.height = '44px';
+    });
+}
+closeSvg[0].addEventListener('click', () => {
+    memesBlock[0].style.height = '44px';
+    closeSvg[0].style.transform = 'rotate(0deg)';
+    for (let i = 0; i < memesBlock.length; i++) {
+        memesBlock[i].addEventListener('mouseenter', () => {
+            memesBlock[i].style.height = '500px';
+        });
+        memesBlock[i].addEventListener('mouseleave', () => {
+            memesBlock[i].style.height = '44px';
+        });
+        closeSvg[i].addEventListener('click', () => {
+            memesBlock[i].style.height = '44px';
+        });
+    }
+});
