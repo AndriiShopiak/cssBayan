8afb66a6022100f21d0d6c619a3959fe31244f23
Andrii Shopiak <andriyshopyak@gmail.com>
2023-03-11 15:43:10 +0200

    feat: open tab when clicked  (Sat, Mar 11, 2023 15:41:35)

--- cssBayan/js/main.js
+++ cssBayan/js/main.js
@@ -1,34 +1,59 @@
 let memesBlock = document.querySelectorAll(".memes__content");
 let closeSvg = document.querySelectorAll("svg");
-for (let i = 1; i < memesBlock.length; i++) {
-    memesBlock[i].addEventListener('mouseenter', () => {
-        memesBlock[i].style.height = '500px';
-        closeSvg[i].style.transform = 'rotate(45deg)';
-    });
-    memesBlock[i].addEventListener('mouseleave', () => {
-        memesBlock[i].style.height = '44px';
-        closeSvg[i].style.transform = 'rotate(0deg)';
-    });
-    closeSvg[i].addEventListener('click', () => {
-        memesBlock[i].style.height = '44px';
-        closeSvg[i].style.transform = 'rotate(0deg)';
+let memesBlockFirst = document.querySelector("#first-item");
+memesBlockFirst.style.height = '500px';
+
+function addActive (index) {
+    memesBlockFirst.style.height = '44px';
+    for(let i = 0; i < memesBlock.length; i++) {
+        memesBlock[i].classList.remove('active');
+    }
+    memesBlock[index].classList.toggle('active');
+}
+for (let i = 0; i < memesBlock.length; i++) {
+    memesBlock[i].addEventListener('click', () => {
+        addActive(i);
     });
 }
-closeSvg[0].addEventListener('click', () => {
-    memesBlock[0].style.height = '44px';
-    closeSvg[0].style.transform = 'rotate(0deg)';
-    for (let i = 0; i < memesBlock.length; i++) {
-        memesBlock[i].addEventListener('mouseenter', () => {
-            memesBlock[i].style.height = '500px';
-            closeSvg[i].style.transform = 'rotate(45deg)';
-        });
-        memesBlock[i].addEventListener('mouseleave', () => {
-            memesBlock[i].style.height = '44px';
-            closeSvg[i].style.transform = 'rotate(0deg)';
-        });
-        closeSvg[i].addEventListener('click', () => {
-            memesBlock[i].style.height = '44px';
-            closeSvg[i].style.transform = 'rotate(0deg)';
-        });
-    }
-});
+
+
+
+// for (let i = 1; i < memesBlock.length; i++) {
+//     memesBlock[i].addEventListener('mouseenter', () => {
+//         memesBlock[i].style.height = '500px';
+//         closeSvg[i].style.transform = 'rotate(45deg)';
+//     });
+//     memesBlock[i].addEventListener('mouseleave', () => {
+//         memesBlock[i].style.height = '44px';
+//         closeSvg[i].style.transform = 'rotate(0deg)';
+//     });
+//     closeSvg[i].addEventListener('click', () => {
+//         memesBlock[i].style.height = '44px';
+//         closeSvg[i].style.transform = 'rotate(0deg)';
+//     });
+// }
+// closeSvg[0].addEventListener('click', () => {
+//     memesBlock[0].style.height = '44px';
+//     closeSvg[0].style.transform = 'rotate(0deg)';
+//     for (let i = 0; i < memesBlock.length; i++) {
+//         memesBlock[i].addEventListener('mouseenter', () => {
+//             memesBlock[i].style.height = '500px';
+//             closeSvg[i].style.transform = 'rotate(45deg)';
+//         });
+//         memesBlock[i].addEventListener('mouseleave', () => {
+//             memesBlock[i].style.height = '44px';
+//             closeSvg[i].style.transform = 'rotate(0deg)';
+//         });
+//         closeSvg[i].addEventListener('click', () => {
+//             memesBlock[i].style.height = '44px';
+//             closeSvg[i].style.transform = 'rotate(0deg)';
+//         });
+//     }
+// });
+// for (let i = 1; i < memesBlock.length; i++) {
+//     memesBlock[i].addEventListener('click', () => {
+//         memesBlock[i].classList.remove('active');
+//         memesBlock[i].style.height = '44px';
+//         memesBlock[i].classList.toggle('active');
+//     });
+// }
