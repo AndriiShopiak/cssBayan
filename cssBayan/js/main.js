0ad6882d1edf40326b3899a55d08429f12216108
Andrii <andriyshopyak@gmail.com>
2023-03-11 17:54:23 +0200

    feat: created close func  (Sat, Mar 11, 2023 17:52:45)

--- cssBayan/js/main.js
+++ cssBayan/js/main.js
@@ -10,50 +10,13 @@ function addActive (index) {
     }
     memesBlock[index].classList.toggle('active');
 }
+function closeItem(index) {
+    memesBlock[index].classList.toggle('active');
+}
 for (let i = 0; i < memesBlock.length; i++) {
     memesBlock[i].addEventListener('click', () => {
         addActive(i);
     });
+    
 }
 
-
-
-// for (let i = 1; i < memesBlock.length; i++) {
-//     memesBlock[i].addEventListener('mouseenter', () => {
-//         memesBlock[i].style.height = '500px';
-//         closeSvg[i].style.transform = 'rotate(45deg)';
-//     });
-//     memesBlock[i].addEventListener('mouseleave', () => {
-//         memesBlock[i].style.height = '44px';
-//         closeSvg[i].style.transform = 'rotate(0deg)';
-//     });
-//     closeSvg[i].addEventListener('click', () => {
-//         memesBlock[i].style.height = '44px';
-//         closeSvg[i].style.transform = 'rotate(0deg)';
-//     });
-// }
-// closeSvg[0].addEventListener('click', () => {
-//     memesBlock[0].style.height = '44px';
-//     closeSvg[0].style.transform = 'rotate(0deg)';
-//     for (let i = 0; i < memesBlock.length; i++) {
-//         memesBlock[i].addEventListener('mouseenter', () => {
-//             memesBlock[i].style.height = '500px';
-//             closeSvg[i].style.transform = 'rotate(45deg)';
-//         });
-//         memesBlock[i].addEventListener('mouseleave', () => {
-//             memesBlock[i].style.height = '44px';
-//             closeSvg[i].style.transform = 'rotate(0deg)';
-//         });
-//         closeSvg[i].addEventListener('click', () => {
-//             memesBlock[i].style.height = '44px';
-//             closeSvg[i].style.transform = 'rotate(0deg)';
-//         });
-//     }
-// });
-// for (let i = 1; i < memesBlock.length; i++) {
-//     memesBlock[i].addEventListener('click', () => {
-//         memesBlock[i].classList.remove('active');
-//         memesBlock[i].style.height = '44px';
-//         memesBlock[i].classList.toggle('active');
-//     });
-// }
